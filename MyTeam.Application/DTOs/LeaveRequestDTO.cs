41185bba8150693ed7e674b065bb06d7d2339e8e
bruno_mocho <170221059@estudantes.ips.pt>
2025-07-29 10:59:09 +0100

    Add Application -> DTOs classes strucure

---
+++ MyTeam.Application/DTOs/LeaveRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTeam.Application.DTOs
+{
+    internal class LeaveRequestDTO
+    {
+    }
+}
