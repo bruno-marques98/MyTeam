5c322473bace80089ea30ca51ceb7a016360534c
bruno_mocho <170221059@estudantes.ips.pt>
2025-08-11 17:34:36 +0100

    Domain -> Add Benefit class and logic

---
+++ MyTeam.Domain/Entities/Benefit.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MyTeam.Domain.Entities
+{
+    internal class Benefit
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Amount { get; private set; }
+
+        protected Benefit() { }
+
+        public Benefit(string name, string description, decimal amount)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Amount = amount;
+        }
+    }
+}
