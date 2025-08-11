d25b747d645ec5a914ffc2356951963d8496fafa
bruno_mocho <170221059@estudantes.ips.pt>
2025-08-11 17:30:23 +0100

    Domain -> Added Training class and logic

---
+++ MyTeam.Domain/Entities/Training.cs
@@ -0,0 +1,32 @@
+﻿
+namespace MyTeam.Domain.Entities
+{
+    internal class Training
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+
+        public ICollection<Employee> Participants { get; private set; }
+
+        protected Training() { }
+
+        public Training(string title, string description, DateTime startDate, DateTime endDate)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+            Participants = new List<Employee>();
+        }
+
+        public void AddParticipant(Employee employee)
+        {
+            if (!Participants.Contains(employee))
+                Participants.Add(employee);
+        }
+    }
+}
