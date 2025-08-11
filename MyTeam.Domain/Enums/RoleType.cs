bcff714dd6bbb854aee32216b9a5f1b836d7f338
bruno_mocho <170221059@estudantes.ips.pt>
2025-08-11 17:22:33 +0100

    Domain -> Removed unused usings and add public type of class to enums

--- MyTeam.Domain/Enums/RoleType.cs
+++ MyTeam.Domain/Enums/RoleType.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MyTeam.Domain.Enums
 {
-    enum RoleType
+    public enum RoleType
     {
         Consultant,
         TeamLeader,
