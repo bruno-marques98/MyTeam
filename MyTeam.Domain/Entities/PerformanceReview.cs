08f3b991c49540f7f3f03997950f95a68a976bad
bruno_mocho <170221059@estudantes.ips.pt>
2025-08-11 17:32:28 +0100

    Domain -> Added PerformanceReview class and logic

---
+++ MyTeam.Domain/Entities/PerformanceReview.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MyTeam.Domain.Entities
+{
+    internal class PerformanceReview
+    {
+        public Guid Id { get; private set; }
+        public Guid EmployeeId { get; private set; }
+        public Employee Employee { get; private set; }
+        public DateTime ReviewDate { get; private set; }
+        public string Reviewer { get; private set; }
+        public string Comments { get; private set; }
+        public int Rating { get; private set; } // Example: 1-5 scale
+
+        protected PerformanceReview() { }
+
+        public PerformanceReview(Employee employee, DateTime reviewDate, string reviewer, string comments, int rating)
+        {
+            Id = Guid.NewGuid();
+            Employee = employee;
+            EmployeeId = employee.Id;
+            ReviewDate = reviewDate;
+            Reviewer = reviewer;
+            Comments = comments;
+            Rating = rating;
+        }
+    }
+}
