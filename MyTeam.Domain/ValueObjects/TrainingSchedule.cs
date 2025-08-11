5ba4b11fcd5866a3027bc4ef7830908f35e4e99f
bruno_mocho <170221059@estudantes.ips.pt>
2025-08-11 17:37:45 +0100

    Domain -> Add TrainingSchedule class and logic

---
+++ MyTeam.Domain/ValueObjects/TrainingSchedule.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MyTeam.Domain.ValueObjects
+{
+    public class TrainingSchedule
+    {
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+
+        public TrainingSchedule(DateTime startDate, DateTime endDate)
+        {
+            if (endDate < startDate)
+                throw new ArgumentException("End date cannot be earlier than start date.");
+
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
