d25c25de2840f2113e61c2c02d0075d3b80aab00
bruno_mocho <170221059@estudantes.ips.pt>
2025-08-11 17:39:19 +0100

    Domain -> Add ReviewScoreReviewScore class and logic

---
+++ MyTeam.Domain/ValueObjects/ReviewScore.cs
@@ -0,0 +1,15 @@
+﻿namespace MyTeam.Domain.ValueObjects
+{
+    public class ReviewScore
+    {
+        public int Value { get; }
+
+        public ReviewScore(int value)
+        {
+            if (value < 1 || value > 5)
+                throw new ArgumentException("Score must be between 1 and 5.");
+
+            Value = value;
+        }
+    }
+}
