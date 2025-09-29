5254ed777e0df8a8c238e151197ba7d52813b3e0
bruno-marques98 <bruno98silva@hotmail.com>
2025-09-29 10:24:14 +0100

    Application -> Added Reviewer property on PerformanceReviewDto

--- MyTeam.Application/DTOs/PerformanceReviewDto.cs
+++ MyTeam.Application/DTOs/PerformanceReviewDto.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public Guid EmployeeId { get; set; }
         public DateTime ReviewDate { get; set; }
+        public string Reviewer { get; set; } 
         public string Comments { get; set; }
         public int Rating { get; set; }
     }
