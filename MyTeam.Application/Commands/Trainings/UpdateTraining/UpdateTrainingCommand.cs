8e1542df74e516968dc2d18999a47fe0e0d966e2
bruno-marques98 <bruno98silva@hotmail.com>
2025-09-30 09:57:05 +0100

    Application -> Added UpdateTraining command

---
+++ MyTeam.Application/Commands/Trainings/UpdateTraining/UpdateTrainingCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using MyTeam.Application.DTOs;
+
+namespace MyTeam.Application.Commands.Trainings.UpdateTraining
+{
+    public class UpdateTrainingCommand : IRequest<TrainingDto>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ MyTeam.Application/Commands/Trainings/UpdateTraining/UpdateTrainingHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyTeam.Application.DTOs;
+using MyTeam.Application.Interfaces.Common;
+
+namespace MyTeam.Application.Commands.Trainings.UpdateTraining
+{
+    public class UpdateTrainingHandler : IRequestHandler<UpdateTrainingCommand, TrainingDto>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateTrainingHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TrainingDto> Handle(UpdateTrainingCommand request, CancellationToken cancellationToken)
+        {
+            var training = await _context.Trainings.FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken);
+
+            if (training == null)
+                return null;
+
+            training.GetType().GetProperty("Title")!.SetValue(training, request.Title);
+            training.GetType().GetProperty("Description")!.SetValue(training, request.Description);
+            training.GetType().GetProperty("StartDate")!.SetValue(training, request.StartDate);
+            training.GetType().GetProperty("EndDate")!.SetValue(training, request.EndDate);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new TrainingDto
+            {
+                Id = training.Id,
+                Title = training.Title,
+                Description = training.Description,
+                StartDate = training.StartDate,
+                EndDate = training.EndDate
+            };
+        }
+    }
+}
