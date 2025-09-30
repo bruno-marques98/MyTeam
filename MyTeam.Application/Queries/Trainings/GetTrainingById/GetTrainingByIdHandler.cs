98c4bbdaafacc35ad1dbae833cd9bb746173e22c
bruno-marques98 <bruno98silva@hotmail.com>
2025-09-30 10:28:01 +0100

    Application -> Added GetTrainingById query

---
+++ MyTeam.Application/Queries/Trainings/GetTrainingById/GetTrainingByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyTeam.Application.DTOs;
+using MyTeam.Application.Interfaces.Common;
+
+namespace MyTeam.Application.Queries.Trainings.GetTrainingById
+{
+    public class GetTrainingByIdHandler : IRequestHandler<GetTrainingByIdQuery, TrainingDto>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetTrainingByIdHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TrainingDto> Handle(GetTrainingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var training = await _context.Trainings.FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken);
+
+            if (training == null)
+                return null;
+
+            return new TrainingDto
+            {
+                Id = training.Id,
+                Title = training.Title,
+                Description = training.Description,
+                StartDate = training.StartDate,
+                EndDate = training.EndDate
+            };
+        }
+    }
+}
---
+++ MyTeam.Application/Queries/Trainings/GetTrainingById/GetTrainingByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MyTeam.Application.DTOs;
+
+namespace MyTeam.Application.Queries.Trainings.GetTrainingById
+{
+    public class GetTrainingByIdQuery : IRequest<TrainingDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
