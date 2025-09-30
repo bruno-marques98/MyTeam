c36841b99d23ecf052c444795e60e7728ecbf1c1
bruno-marques98 <bruno98silva@hotmail.com>
2025-09-30 09:47:46 +0100

    Application -> Added command to create Training

---
+++ MyTeam.Application/Commands/Trainings/CreateTraining/CreateTrainingCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using MyTeam.Application.DTOs;
+
+namespace MyTeam.Application.Commands.Trainings.CreateTraining
+{
+    public class CreateTrainingCommand : IRequest<TrainingDto>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ MyTeam.Application/Commands/Trainings/CreateTraining/CreateTrainingHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using MyTeam.Application.DTOs;
+using MyTeam.Application.Interfaces.Common;
+using MyTeam.Domain.Entities;
+
+namespace MyTeam.Application.Commands.Trainings.CreateTraining
+{
+    public class CreateTrainingHandler : IRequestHandler<CreateTrainingCommand, TrainingDto>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateTrainingHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TrainingDto> Handle(CreateTrainingCommand request, CancellationToken cancellationToken)
+        {
+            var training = new Training(
+                request.Title,
+                request.Description,
+                request.StartDate,
+                request.EndDate
+            );
+
+            _context.Trainings.Add(training);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new TrainingDto
+            {
+                Id = training.Id,
+                Title = training.Title,
+                Description = training.Description,
+                StartDate = training.StartDate,
+                EndDate = training.EndDate
+            };
+        }
+    }
+}
